8e82842a01901b1e57ee2ade7433196b2b6370bd
feportoa <feporto.a@gmail.com>
2023-04-22 20:34:12 -0300

    Added comments and 1 extra item on README.md checklist

--- README.md
+++ README.md
@@ -4,7 +4,8 @@
 - [x] Return service value,
 - [x] Open this repository if the user chooses to proceed to "Payment",
 - [ ] Open web browsers in both Windows and Apple systems,
+- [ ] Create functions to repeate failed processes instead of ending the program with a return 1 statement
 - [ ] Assyncronous Threading that allows th web browser and the program to run simultaneously and without interference from each another,
-- [ ] ADD COMMENTS TO THE CODE!
+- [x] ADD COMMENTS TO THE CODE!
 
 This code was written for testing purpose. In the moment I'm writing this (04/22/2023 | 18:54), I'm learning C++ with a Udemy course ([Check here!](https://www.udemy.com/course/beginning-c-plus-plus-programming/){:target="_blank"}). I'm on section 6 and class 50. I'm also using ChatGPT to boost my learning and to explain me some specific topics during the course.
--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
+#include <cmath>
 
 const int chargePerRoom {30};
-const float salesTax {1.06};
+const float salesTax {1.06}; // 6% fee (6 /100 = 0.06 + 100% = 1.06)
 
 float Rooms(unsigned int rooms);
 std::string toUpperCase(std::string str);
@@ -19,42 +20,51 @@ int main()
     std::cin >> userChosenOption;
 
     system("clear");
-    if(userChosenOption == 1){
+    // Process the option chosen by the user
+    if(userChosenOption == 1){ // Problems handling float/double injection
         unsigned int clientNumberOfRooms {0}; 
         float serviceValue {0.0F};
         std::string userPaymentConfirmation {"Y"};
 
         std::cout << "Type the number of rooms you need a Ultra Carpet Clean: ";
-        if(std::cin >> clientNumberOfRooms){
+
+        // Checks if clientNumberOfRooms is an int and then proceeds
+        if(std::cin >> clientNumberOfRooms){ // Problems handling float/double injection
             serviceValue = Rooms(clientNumberOfRooms);
             std::cout << "The total value is: " << serviceValue << ". Would you like to confirm?" << std::endl;
             std::cout << "Yes[Y] or No[N]" << std::endl;
             
+            // Checks if user payment confirmation is Y or YES to proceed to payment session
             if(std::cin >> userPaymentConfirmation){
                 if(toUpperCase(userPaymentConfirmation) == "Y" || toUpperCase(userPaymentConfirmation) == "YES"){
                     std::cout << "Redirecting user..." << std::endl;
                     system("xdg-open https://github.com/feportoa");
                 } else if(toUpperCase(userPaymentConfirmation) == "N" || toUpperCase(userPaymentConfirmation) == "NO") {
+                    // Ends the program in case of N or NO
                     std::cout << "Closing process" << std::endl;
                     return 0;
+                    
                 } else {
+                    // Return an error and closes the program if option is invalid
                     std::cout << "[ERROR] Invalid Option. Closing Application" << std::endl;
                     return 1;
                 }
 
             } else {
+                // Returns an error if the user does not use a string or char
                 std::cout << "[ERROR] Invalid Input!\nClosing Application" << std::endl;
                 return 1;
             }
 
         } else {
+            // Returns an error if the user does not use an int
             std::cout << "[ERROR] Invalid Input!\nClosing Application." << std::endl;
             return 1;
         }
         
     } else if(userChosenOption == 2){
         std::cout << "Well, we don't really have a costumer support, so we'll just end the chat :)\nHave a great day!" << std::endl;
-    } else {
+    } else { // if user chooses an unexisting option, or use anything but an int, ends the program with an error
         std::cout << "Invalid input, try again another time." << std::endl;
         return 1;
     }
@@ -62,14 +72,17 @@ int main()
     return 0;
 }
 
+// Calculate the total value of the service for an specific number of rooms
 float Rooms(unsigned int rooms)
 {
     float value {0};
 
+    // Multiplies the number of rooms with charge per room them applies a fee of 6%
     value = (rooms * chargePerRoom) * salesTax;
     return value;
 }
 
+// Sets strings to upper case (as said in the name)
 std::string toUpperCase(std::string str)
 {
     for(char& c : str){
